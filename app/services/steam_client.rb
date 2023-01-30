0b8d26a6defcf8834c02c4d1a5fe461226d50a8b
riul <riul.souza@nuuvem.com>
2023-01-30 13:41:52 -0400

    WIP: put steam data in view

--- app/services/steam_client.rb
+++ app/services/steam_client.rb
@@ -3,12 +3,15 @@ class SteamClient
   base_uri 'store.steampowered.com/api'
 
   def initialize(app_id)
+    @app_id = app_id
     @options = { query: { appids: app_id }, format: 'json' }
   end
 
   def game_info
     response = self.class.get("/appdetails", @options)
 
-    JSON.parse(response.body)
+    json = JSON.parse(response.body)
+
+    json[@app_id]
   end
 end
